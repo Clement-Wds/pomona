78c1db1fd6ae829475ce038c40c04224cc762fad
Clement-Wds <clementwalshdeserrant@mail.com>
2021-02-22 21:42:51 +0100

    cloudinary 3

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     upload: {
         provider: 'cloudinary',
         providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
+            cloud_name: env('CLOUDINARY_NAME', 'derprl9h5'),
+            api_key: env.int('CLOUDINARY_KEY', 238378572495487),
+            api_secret: env('CLOUDINARY_SECRET', 'YBwfVckEHsvbvsXFyd4Qps0AMog'),
         },
     },
 });
