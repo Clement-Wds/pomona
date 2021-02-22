fc2eb3b60e98d9320fcb96619f44ea8f0ba0000b
Clement-Wds <clementwalshdeserrant@mail.com>
2021-02-22 17:04:21 +0100

    database

--- config/database.js
+++ config/database.js
@@ -5,18 +5,14 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.username,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false
-        }
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
-      options: {
-        ssl: true,
-      },
+      options: {}
     },
   },
 });
